87fb326cc187ee19726c681c5a09b3c37467f06d
CS23students <153345000+CS23students@users.noreply.github.com>
2024-08-05 11:33:26 +0530

    Update 12-employee-payroll.c

--- 12-employee-payroll.c
+++ 12-employee-payroll.c
@@ -16,39 +16,49 @@ struct
     float exclusion;
     float totalSalary;
     float netSalary;
-} emp;
+} emp[5];
 
 int main()
 {
 
-    printf("Enter Employee Name: ");
-    scanf("%s", emp.name);
-    printf("Enter Basic Pay: ");
-    scanf("%f", &emp.basicPay);
-    printf("Enter Employee ID: ");
-    scanf("%d", &emp.empId);
+    int i, n;
 
-    // Calculate allowances and deductions
-    emp.hra = 0.18 * emp.basicPay;
-    emp.da = 0.15 * emp.basicPay;
-    emp.pf = 0.10 * emp.basicPay;
-    emp.lic = 0.07 * emp.basicPay;
-    emp.exclusion = emp.pf + emp.lic;
-    emp.totalSalary = emp.basicPay + emp.hra + emp.da;
-    emp.netSalary = emp.totalSalary - emp.exclusion;
+    printf("Enter the number of employees: ");
+    scanf("%d", &n);
+    for (i = 0; i < n; i++)
+    {
+
+        printf("Enter Employee Name: ");
+        scanf("%s", emp[i].name);
+        printf("Enter Employee ID: ");
+        scanf("%d", &emp[i].empId);
+        printf("Enter Basic Pay: ");
+        scanf("%f", &emp[i].basicPay);
+
+        // Calculate allowances and deductions
+        emp[i].hra = 0.18 * emp[i].basicPay;
+        emp[i].da = 0.15 * emp[i].basicPay;
+        emp[i].pf = 0.10 * emp[i].basicPay;
+        emp[i].lic = 0.07 * emp[i].basicPay;
+        emp[i].exclusion = emp[i].pf + emp[i].lic;
+        emp[i].totalSalary = emp[i].basicPay + emp[i].hra + emp[i].da;
+        emp[i].netSalary = emp[i].totalSalary - emp[i].exclusion;
+    }
 
     // Print the payroll details
     printf("\nEmployee Details:\n");
-    printf("Name: %s\n", emp.name);
-    printf("Employee ID: %d\n", emp.empId);
-    printf("Basic Pay: %.2f\n", emp.basicPay);
-    printf("HRA (18%% of BP): %.2f\n", emp.hra);
-    printf("DA (15%% of BP): %.2f\n", emp.da);
-    printf("PF (10%% of BP): %.2f\n", emp.pf);
-    printf("LIC (7%% of BP): %.2f\n", emp.lic);
-    printf("Exclusion (PF + LIC): %.2f\n", emp.exclusion);
-    printf("Total Salary (BP + HRA + DA): %.2f\n", emp.totalSalary);
-    printf("Net Salary (Total Salary - Exclusion): %.2f\n", emp.netSalary);
+    printf("Name\tID\tBasic Pay\tHRA\t\tDA\t\tPF\tLIC\tExclusion\t\tTotal Salary\tNet Salary\n");
+    for (i = 0; i < n; i++)
+    {
+        printf("%s\t%d\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\n", emp[i].name, emp[i].empId, emp[i].basicPay, emp[i].hra, emp[i].da, emp[i].pf, emp[i].lic, emp[i].exclusion, emp[i].totalSalary, emp[i].netSalary);
+    }
 
     return 0;
 }
+
+/*Sample OUTPUT:
+Employee Details:
+Name      ID      Basic Pay       HRA       DA       PF       LIC      Exclusion       Total Salary    Net Salary
+John      1       10000.00        1800.00  1500.00  1000.00  700.00   1700.00         13300.00        11600.00
+Jack      2       20000.00        3600.00  3000.00  2000.00  1400.00  3400.00         26600.00        23200.00
+*/
